2dbfb7be5f367b151eb49882ef00ae113146d901
geovannadama <127845470+geovannadama@users.noreply.github.com>
2024-06-03 12:49:25 -0300

    Adiciona nova tela de login

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   
+
+</head>
+<body class="d-flex flex-column h-100 text-center text-white" style="background-color:   #002366;">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto">
+        <div>
+          <h3 class="float-md-start mb-0 text-white">Inicio</h3>
+          <nav class="nav nav-masthead justify-content-center float-md-end">
+            <div class="btn-group">
+                <a class="nav-link active btn btn-secondary" aria-current="page" href="index.php">Home</a>
+                <a class="nav-link btn btn-secondary" href="login.php">Login</a>
+                <a class="nav-link btn btn-secondary" href="cadastrar.php">Cadastro</a>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+      <main class="px-3">
+        <h1>Bem-vindo à aplicação PHP.</h1>
+        <p class="lead">Página inicial (home) de um projeto criado para ensino de base de dados utilizando a linguagem PHP como linguagem de back-end. Acesse o projeto clicando em:</p>
+        <a href="https://github.com/geovannadama/cadastro_login" class="btn btn-primary">GitHub</a>
+      </main>
+
+      <footer class="mt-auto text-white-50" style="background-color:  #002366; padding: 20px;">
+        <p>Aplicação PHP com Bootstrap 5, by <a href="https://getbootstrap.com/" style="color: white;">Bootstrap</a>.</p>
+    </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+9b5U5fR5z0d3N5XyFw5p6C5MQf6D" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+   
